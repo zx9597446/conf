919d344528f849dd3eb8c813e99bbb12323e027b
zx9597446 <zx9597446@gmail.com>
2014-03-31 11:35:18 +0800

    init commit

---
+++ conf.go
@@ -0,0 +1,35 @@
+//load json config file to struct,
+//create file if not exist
+package conf
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"os"
+)
+
+//load config file to struct
+func Load(filename string, result interface{}) error {
+	file, err := os.Open(filename)
+	defer file.Close()
+	if err != nil {
+		cf, err := os.Create(filename)
+		defer cf.Close()
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+		data, err := json.MarshalIndent(result, "", "  ")
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+		cf.Write(data)
+		msg := "config file not exist, created"
+		log.Println(msg)
+		return errors.New(msg)
+	}
+	dec := json.NewDecoder(file)
+	return dec.Decode(&result)
+}
